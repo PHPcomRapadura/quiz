cf63bab5f178de9142d11ff1941fca875885dee6
William Correa <wilcorrea@gmail.com>
2024-04-08 08:35:36 -0300

    feature: configure CI/CD

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/quiz">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
