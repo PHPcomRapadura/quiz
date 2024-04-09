672666995824e3813d43a8e45dc1093ed729fa58
William Correa <wilcorrea@gmail.com>
2024-04-09 02:55:12 -0300

    feature: improve UI

--- src/view/components/game/GameList.tsx
+++ src/view/components/game/GameList.tsx
@@ -18,10 +18,7 @@ export function GameList ({ games }: { games: Game[] }) {
           <div className={styles.GameList}>
             {games.map((game: Game) => (
               <AlertPrimary key={game.id}>
-                <Link
-                  className="text-light-emphasis"
-                  to={`/game/${game.id}/play`}
-                >
+                <Link to={`/game/${game.id}/play`}>
                   {game.description}
                 </Link>
               </AlertPrimary>
